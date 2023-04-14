c539d482398d1e4319cab3cbcef6cca45829e2db
sayurihdz <sayuhdz.richizv@outlook.com>
2023-04-14 10:00:30 -0600

    tarea 05

--- 06-JS-V/homework/homework.js
+++ 06-JS-V/homework/homework.js
@@ -8,12 +8,24 @@ function crearUsuario() {
   // {{nombre}} debe ser el nombre definido en cada instancia
   // Devuelve la clase
   // Tu código:
+  function Usuario(usuario, nombre, email , password){
+ this.usuario = usuario;
+ this.nombre = nombre ;
+ this.email = email;
+ this.password = password;
+ this.saludar = fuction () 
+  return 'Hola, mi nombre es' + this.nombre
+ }
+  
+  return Usuario; 
+ 
 }
 
 function agregarMetodoPrototype(Constructor) {
   // Agrega un método al Constructor del `prototype`
   // El método debe llamarse "saludar" y debe devolver la string "Hello World!"
   // Tu código:
+
 }
 
 function agregarStringInvertida() {
